056d8a81349554225abafc0e6bb77628b29dc5d9
Kelly <keortizf@gmail.com>
2022-11-24 23:49:25 -0300

    Adicionado código para adicionar o personagem e o chão

--- Controller/GameViewController.swift
+++ Controller/GameViewController.swift
@@ -10,22 +10,22 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad(){
-            super.viewDidLoad()
-            let scene = GameScene(size: CGSize(width: 2048, height: 1536))
-            scene.scaleMode = .aspectFill
-            
-            let skView = view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            skView.showsPhysics = true
-            skView.presentScene(scene)
-        }
+        super.viewDidLoad()
+        super.viewDidLoad()
+        let scene = GameScene(size: CGSize(width: 2048, height: 1536))
+        scene.scaleMode = .aspectFill
+        
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.showsPhysics = true
+        skView.presentScene(scene)
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
 }
+
